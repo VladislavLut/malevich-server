0d0707e3c4687246565e3cc0ad287d11a7009aac
Vladislav <vladislav5640@gmail.com>
2018-04-15 11:52:08 +0300

    Replaced 4 different find() methods by one sql query method

--- src/main/java/com/malevich/server/repository/ReservedRepository.java
+++ src/main/java/com/malevich/server/repository/ReservedRepository.java
@@ -2,19 +2,32 @@ package com.malevich.server.repository;
 
 import com.malevich.server.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ReservedRepository extends JpaRepository<Reservation, Integer> {
 
-//    Optional<Reservation> findByPhoneAndDate(String phone, String date);
-//
-//    List<Reservation> findAllByNameAndDate(String name, String date);
-//
-//    List<Reservation> findAllByDateAndTime(String date, String time);
-//
-//    List<Reservation> findAllByDate(String date);
+    @Query("select r " +
+            "from Reservation r " +
+            "where r.date like %:date% " +
+            "and r.time like %:time% " +
+            "and r.name like %:name% " +
+            "and r.phone like %:phone%")
+    Optional<List<Reservation>> selectByParams(
+            @Param("date") String date,
+            @Param("time") String time,
+            @Param("name") String name,
+            @Param("phone") String phone);
+    /*
+     * There is no need in update query for Reserved table
+     * */
 
-
+//    @Modifying(clearAutomatically = true)
+//    @Query("update Reservation r set ")
+//    int updateReservation();
 }
